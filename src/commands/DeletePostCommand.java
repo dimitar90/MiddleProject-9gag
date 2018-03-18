c82017951af02a09a89d4f1d0e0686f80eda76a7
Dimitar N <dimitar90nikolov@gmail.com>
2018-03-18 10:00:27 +0200

    Delete post command finished

--- src/commands/DeletePostCommand.java
+++ src/commands/DeletePostCommand.java
@@ -3,17 +3,34 @@ package commands;
 import java.io.IOException;
 import java.util.List;
 
+import exceptions.PostException;
 import exceptions.UserException;
+import repositories.PostRepository;
+import utils.Checker;
+import utils.Session;
 
 public class DeletePostCommand extends Command{
+
+	private static final int ARGUMENT_LENGTH = 1;
+	private static final String FAILED_LOGIN_MESSAGE = "Login first";
+	private static final String SUCCESSFULLY_MESSAGE = "Delete post successfully";
 
 	public DeletePostCommand(List<String> data) {
 		super(data);
 	}
 
 	@Override
-	public String execute() throws UserException, IOException {
-		String result = "delete post!";
-		return result;
+	public String execute() throws UserException, IOException, PostException {
+		if (!Checker.isValidDateLenght(this.getData(), ARGUMENT_LENGTH)) {
+			throw new PostException(Command.INVALID_DATA);
+		}
+		
+		if (Session.getInstance().getUser() == null) {
+			throw new UserException(FAILED_LOGIN_MESSAGE);
+		}
+		
+		int postId = Integer.parseInt(this.getData().get(0));
+		PostRepository.getInstance().delete(postId);
+		return SUCCESSFULLY_MESSAGE;
 	}
 }
