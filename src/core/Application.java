f078e69bad17ea0a50235f8f04d0336a083cd280
Dimitar N <dimitar90nikolov@gmail.com>
2018-03-24 08:51:30 +0200

    set thread to damon

--- src/core/Application.java
+++ src/core/Application.java
@@ -2,6 +2,8 @@ package core;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import exceptions.SectionException;
 import models.Comment;
@@ -14,25 +16,33 @@ import repositories.PostRepository;
 import repositories.SectionRepository;
 import repositories.TagRepository;
 import repositories.UserRepository;
+import utils.Downloader;
 
 public class Application {
+	private static final String MESSAGE_ITERRUPT_EXCEPTION = "Thread has been interrupted";
+
+	private static final String[] SECTION_NAMES = { "Travel", "Wallpaper", "Photography", "Animals", "Awesome", "Car",
+			"Comic", "Country", "Crafts", "Food", "Funny", "Gaming", "Girl", "Horror", "Music", "Politics",
+			"Relationship", "Savage", "School", "Sci-Tech", "Superhero", "Sport", "Timely", "Video", "WTF" };
+	private static Downloader downloader = new Downloader();
+
 	public static void main(String[] args) {
+
 		try {
 			File file = new File(SectionRepository.SECTION_PATH);
 			if (!file.exists()) {
-				//тука в то€ метод взех н€какви примерни секции от реални€ сайт и направо ги зареждам в репоситорито
+				// тука в то€ метод взех н€какви примерни секции от реални€ сайт и направо ги
+				// зареждам в репоситорито
 				loadSections();
 			} else {
 				SectionRepository.getInstance().deserialize();
 				Section.setValueToIdGenerator(SectionRepository.getInstance().getLastId());
 			}
-			
+
 			UserRepository.getInstance().deserialize();
-			// UserRepository.getInstance().importUser();
 			User.setValueToIdGenerator(UserRepository.getInstance().getLastId());
 
 			CommentRepository.getInstance().deserialize();
-			// CommentRepository.getInstance().importComment();
 			Comment.setValueToIdGenerator(CommentRepository.getInstance().getLastId());
 
 			TagRepository.getInstance().deserialize();
@@ -40,22 +50,20 @@ public class Application {
 
 			PostRepository.getInstance().deserialize();
 			Post.setValueToIdPostGenerator(PostRepository.getInstance().getLastId());
-			
+
 		} catch (IOException | SectionException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
+		downloader.setDaemon(true);
+		downloader.start();
 
 		Runnable engine = new Engine();
 		engine.run();
 	}
 
 	private static void loadSections() throws SectionException {
-		String[] sectionNames = { "Travel", "Wallpaper", "Photography", "Animals", "Awesome", "Car", "Comic", "Country",
-				"Crafts", "Food", "Funny", "Gaming", "Girl", "Horror", "Music", "Politics", "Relationship", "Savage",
-				"School", "Sci-Tech", "Superhero", "Sport", "Timely", "Video", "WTF" };
-		
-		for (String name : sectionNames) {
+		for (String name : SECTION_NAMES) {
 			SectionRepository.getInstance().addSection(name);
 		}
 	}
