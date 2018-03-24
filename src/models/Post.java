fb262e91ef2ed3bd2d931c5290a624dc46137e5e
Dimitar N <dimitar90nikolov@gmail.com>
2018-03-24 08:55:53 +0200

    add method downloadImage()

--- src/models/Post.java
+++ src/models/Post.java
@@ -1,5 +1,12 @@
 package models;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
@@ -11,11 +18,15 @@ import exceptions.PostException;
 import repositories.TagRepository;
 import utils.IDeserialize;
 
-public class Post implements IDeserialize{
+public class Post implements IDeserialize {
+	private static final String FINAL_MESSAGE = "Download completed. . .";
+	private static final int MULTIPLYER = 100;
+	private static final double INITIAL_PERCENT_OF_DOWNLOAD = 0.00;
+	private static final int KB = 1024;
 	private static final String MESSAGE_INVALID_DESCRIPTION = "Give a funny, creative and descriptive title to the post would give the post a boost!";
-	
 	private static final String MESSAGE_INVALID_NAME = "Invalid parameters for name";
-	
+	private static final String PATH_RES = "C:\\Users\\Dimitar\\middleProject-9gag\\MiddleProject-9gag\\resources\\";
+
 	private static int nextPostId;
 	private int id;
 	private String imageUrl;
@@ -26,7 +37,8 @@ public class Post implements IDeserialize{
 	private Set<Integer> tagIds;
 	private List<Integer> ratings;
 	private Section section;
-	
+	private volatile boolean hasDownload;
+
 	static {
 		nextPostId = 0;
 	}
@@ -40,6 +52,7 @@ public class Post implements IDeserialize{
 		this.date = new Date();
 		this.ratings = new ArrayList<>();
 		this.section = section;
+		this.hasDownload = false;
 	}
 
 	public void addComment(int commentId) {
@@ -61,7 +74,7 @@ public class Post implements IDeserialize{
 	public void addTagId(int tagId) {
 		this.tagIds.add(tagId);
 	}
-	
+
 	public Set<Integer> getTagIds() {
 		return Collections.unmodifiableSet(this.tagIds);
 	}
@@ -96,26 +109,74 @@ public class Post implements IDeserialize{
 		return this.ratings.stream().mapToInt(r -> r).sum();
 	}
 
+	public boolean anyComments() {
+		return this.commentIds.size() > 0;
+	}
+
+	public boolean isDownload() {
+		return this.hasDownload;
+	}
+
+	public void setFlag(boolean hasUpload) {
+		this.hasDownload = hasUpload;
+	}
+
 	@Override
 	public String toString() {
 		int rating = this.getRating();
 		TagRepository tagRepository = TagRepository.getInstance();
 		Set<String> tagNames = new HashSet<>();
-		
+
 		for (Integer id : this.tagIds) {
 			Tag tag = tagRepository.getTagById(id);
 			if (tag != null) {
 				tagNames.add(tag.getName());
 			}
 		}
-		
-		return "Post description: " + this.description + ". Author: " + user.getName() + " Content(Url): " + this.imageUrl 
-				+ System.lineSeparator() + "Post rating: " + rating  + " Written on: " + this.date
-				 + " Section: " + this.section.getName()  + " Tags: " + String.join(", ", tagNames);
+
+		return "Post description: " + this.description + ". Author: " + user.getName() + " Content(Url): "
+				+ this.imageUrl + System.lineSeparator() + "Post rating: " + rating + " Written on: " + this.date
+				+ " Section: " + this.section.getName() + " Tags: " + String.join(", ", tagNames);
 	}
 
-	public boolean anyComments() {
-		return this.commentIds.size() > 0;
-	}
+	public void downloadImage() {
 
+		String dirName = this.user.getName();
+		dirName += this.user.getId();
+		File file = new File(PATH_RES + dirName);
+
+		try {
+			if (!file.exists()) {
+				file.mkdirs();
+			}
+
+			URL url = new URL(this.imageUrl);
+			HttpURLConnection http = (HttpURLConnection) url.openConnection();
+			double fileSize = (double) http.getContentLengthLong();
+			BufferedInputStream in = new BufferedInputStream(http.getInputStream());
+			FileOutputStream fos = new FileOutputStream(file + "\\Image" + (this.tagIds) + ".jpg");
+
+			BufferedOutputStream bout = new BufferedOutputStream(fos, KB);
+			byte[] buffer = new byte[KB];
+
+			double downloaded = INITIAL_PERCENT_OF_DOWNLOAD;
+			int read = 0;
+			double percentDownloaded = INITIAL_PERCENT_OF_DOWNLOAD;
+
+			while ((read = in.read(buffer, 0, KB)) >= 0) {
+				bout.write(buffer, 0, read);
+				downloaded += read;
+				percentDownloaded = (downloaded * MULTIPLYER) / fileSize;
+				String percent = String.format("%.2f", percentDownloaded);
+				System.out.println("Downloaded " + percent + "% of a file.");
+			}
+			bout.close();
+			in.close();
+			this.setFlag(true);
+			System.out.println(FINAL_MESSAGE);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
