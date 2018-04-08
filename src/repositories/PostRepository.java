60b7e0b21f9972d0e250a0c83d1132c1c765591a
Dimitar N <dimitar90nikolov@gmail.com>
2018-04-08 19:43:21 +0300

    Modify addGradeToPost

--- src/repositories/PostRepository.java
+++ src/repositories/PostRepository.java
@@ -12,20 +12,19 @@ import java.util.Set;
 
 import connection.DatabaseConnection;
 import exceptions.PostException;
-import exceptions.SectionException;
 import models.Comment;
 import models.Post;
 import models.Section;
 import models.Tag;
+import models.User;
 import utils.Session;
 
 public class PostRepository {
+	private static final String INSERT_POST_RATING_USER_QUERY = "INSERT INTO rating_post_user (post_id, user_id, grade) VALUES(?, ?, ?)";
 	private static final String INSERT_POST_QUERY = "INSERT INTO posts (description, internet_url, date_time, author_id, section_id) VALUES(?, ?, ?, ?, ?)";
 	private static final String INSERT_POST_TAG_QUERY = "INSERT INTO post_tag (post_id,tag_id) VALUES (?,?)";
-	public static final Map<Integer, Post> posts = new HashMap<Integer, Post>();
-
+	public static final Map<Integer, Post> POSTS = new HashMap<Integer, Post>();
 	private static final int DOWN_GRADE = -1;
-
 	private static final int UP_GRADE = 1;
 
 	private static final String POSTS_PATH = "posts.json";
@@ -89,8 +88,7 @@ public class PostRepository {
 	// this.posts.remove(postId);
 	// }
 
-	public Post addPost(String description, String url, String sectionName, List<String> tags)
-			throws PostException, SectionException {
+	public Post addPost(String description, String url, String sectionName, List<String> tags) throws Exception {
 		// Get section by name
 		Section section = this.refToSectionRepo.getSectionByName(sectionName);
 		List<String> allSectionNames = this.refToSectionRepo.getAllSectionNames();
@@ -99,7 +97,7 @@ public class PostRepository {
 			throw new PostException(INVALID_SECTION_NAME + String.join(", ", allSectionNames));
 		}
 
-		// Open connection to insert post
+		// Open connection to insert values into posts table
 		Post post = null;
 		try (PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement(INSERT_POST_QUERY,
 				PreparedStatement.RETURN_GENERATED_KEYS);) {
@@ -136,56 +134,15 @@ public class PostRepository {
 			post.addTag(t);
 		}
 
-		// Open connection to insert post id and tag id into mapping table post_tag
+		// Open connection to insert values for post_tag table
 		try (PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement(INSERT_POST_TAG_QUERY);) {
 			for (Integer id : tagIds) {
 				ps.setInt(1, post.getId());
 				ps.setInt(2, id);
 			}
 		}
-		this.posts.put(post.getId(), post);
+		this.POSTS.put(post.getId(), post);
 		return post;
-	}
-
-	// public void exportPost() throws SerializeException, SerialException {
-	// this.serializer.serialize(this.posts, POSTS_PATH);
-	// }
-
-	// public void deserialize() throws IOException {
-	// File file = new File(POSTS_PATH);
-	//
-	// Gson gson = new GsonBuilder().setPrettyPrinting().create();
-	// StringBuilder sb = new StringBuilder();
-	//
-	// try (Scanner sc = new Scanner(file)) {
-	// while (sc.hasNextLine()) {
-	// String line = sc.nextLine();
-	// sb.append(line);
-	// }
-	// }
-	//
-	// Map<Integer, Post> map = gson.fromJson(sb.toString(), new
-	// TypeToken<Map<Integer, Post>>() {
-	// }.getType());
-	//
-	// this.posts = map;
-	// }
-
-	public int getLastId() {
-		if (this.posts == null || this.posts.size() == 0) {
-			return 0;
-		}
-
-		return this.posts.keySet().stream().sorted((id1, id2) -> Integer.compare(id1, id2)).findFirst().get()
-				.intValue();
-	}
-
-	public Post getPostById(int postId) {
-		if (!this.posts.containsKey(postId)) {
-			return null;
-		}
-
-		return this.posts.get(postId);
 	}
 
 	public void addGradeToPost(int postId, byte grade) throws PostException {
@@ -193,19 +150,48 @@ public class PostRepository {
 			throw new PostException(INVALID_GRADE);
 		}
 
-		if (!this.posts.containsKey(postId)) {
+		if (!this.POSTS.containsKey(postId)) {
 			throw new PostException(NOT_EXIST_POST_MEESAGE);
 		}
 
-		// User user = Session.getInstance().getUser();
-		// if (user.checkForRatedPostById(postId)) {
-		// throw new PostException(ALREADY_RATED_POST_MESSAGE);
-		// }
-		//
-		// this.posts.get(postId).addRating(grade);
-		// user.addRatedPostId(postId);
+		 User user = Session.getInstance().getUser();
+		 if (user.checkForRatedPostByPostId(postId)) {
+		 throw new PostException(ALREADY_RATED_POST_MESSAGE);
+		 }
+		 
+		 Post post = PostRepository.getInstance().getPostById(postId);
+		 post.addRating(grade);
+		 user.addRatedPost(post);
+		 
+		 
+		 int userId = user.getId();
+		 try(PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement(INSERT_POST_RATING_USER_QUERY)){
+			 ps.setInt(1, postId);
+			 ps.setInt(2, userId);
+			 ps.setInt(3, grade);
+			 ps.executeUpdate();
+		 }
 	}
 
+	
+	public int getLastId() {
+		if (this.POSTS == null || this.POSTS.size() == 0) {
+			return 0;
+		}
+
+		return this.POSTS.keySet().stream().sorted((id1, id2) -> Integer.compare(id1, id2)).findFirst().get()
+				.intValue();
+	}
+
+	public Post getPostById(int postId) {
+		if (!this.POSTS.containsKey(postId)) {
+			return null;
+		}
+
+		return this.POSTS.get(postId);
+	}
+
+	
 	public void listPostsByTagName(String tagName) {
 		CommentRepository commentsRepository = CommentRepository.getInstance();
 
@@ -217,7 +203,7 @@ public class PostRepository {
 		// при филтрацията, който по всичките ид-та на тагове в поста бърка в таг
 		// репоситорито и връща имената
 		// на тези тагове и филтрирам само тези постове, които съдържат този таг
-		this.posts.values().stream().filter(p -> getTagNamesByTagIds(p.getTagIds()).contains(tagName))
+		this.POSTS.values().stream().filter(p -> getTagNamesByTagIds(p.getTagIds()).contains(tagName))
 				.sorted((p1, p2) -> p2.getDate().compareTo(p1.getDate()))
 				.forEach(p -> System.out.println(p + System.lineSeparator()
 						+ getPrintedComments(commentsRepository.getCommentsByPostId(p.getId()))
@@ -227,7 +213,7 @@ public class PostRepository {
 	public void listAllPostsSortedByLatest() {
 		CommentRepository commentsRepository = CommentRepository.getInstance();
 
-		this.posts.values().stream().sorted((p1, p2) -> p2.getDate().compareTo(p1.getDate()))
+		this.POSTS.values().stream().sorted((p1, p2) -> p2.getDate().compareTo(p1.getDate()))
 				.forEach(p -> System.out.println(p + System.lineSeparator()
 						+ getPrintedComments(commentsRepository.getCommentsByPostId(p.getId()))
 						+ System.lineSeparator()));
@@ -236,7 +222,7 @@ public class PostRepository {
 	public void listAllPostsSortedByRating() {
 		CommentRepository commentsRepository = CommentRepository.getInstance();
 
-		this.posts.values().stream().sorted((p1, p2) -> Integer.compare(p2.getRating(), p1.getRating()))
+		this.POSTS.values().stream().sorted((p1, p2) -> Integer.compare(p2.getRating(), p1.getRating()))
 				.forEach(p -> System.out.println(p + System.lineSeparator()
 						+ getPrintedComments(commentsRepository.getCommentsByPostId(p.getId()))
 						+ System.lineSeparator()));
@@ -269,7 +255,7 @@ public class PostRepository {
 		if (postIds.size() == 0) {
 			System.out.println(NO_POST_MESSAGE);
 		} else {
-			this.posts.entrySet().stream().filter(kvp -> postIds.contains(kvp.getKey())).map(kvp -> kvp.getValue())
+			this.POSTS.entrySet().stream().filter(kvp -> postIds.contains(kvp.getKey())).map(kvp -> kvp.getValue())
 					.sorted((p1, p2) -> p2.getDate().compareTo(p1.getDate()))
 					.forEach(p -> System.out.println(p + System.lineSeparator()
 							+ getPrintedComments(commentsRepository.getCommentsByPostId(p.getId()))
@@ -309,7 +295,7 @@ public class PostRepository {
 	}
 
 	public void getProcess() {
-		for (Post post : posts.values()) {
+		for (Post post : POSTS.values()) {
 			// && Session.getInstance().getUser() != null
 			// &&Session.getInstance().getUser().getId() == post.getUser().getId()
 			if (!post.isDownload()) {
