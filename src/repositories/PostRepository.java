867a7e3d2f238d92cf3df5ab3409c57395046c74
Dimitar N <dimitar90nikolov@gmail.com>
2018-04-08 20:51:29 +0300

    Modified following: getPostById; listPostsByTagName; listAllPostsSortedByLatestlistAllPostsSortedByRating; listAllPostsBySectionName

--- src/repositories/PostRepository.java
+++ src/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package repositories;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -20,6 +21,7 @@ import models.User;
 import utils.Session;
 
 public class PostRepository {
+	private static final String MSG_SUCCESSFULY_GRADED = "Successfuly graded post";
 	private static final String INSERT_POST_RATING_USER_QUERY = "INSERT INTO rating_post_user (post_id, user_id, grade) VALUES(?, ?, ?)";
 	private static final String INSERT_POST_QUERY = "INSERT INTO posts (description, internet_url, date_time, author_id, section_id) VALUES(?, ?, ?, ?, ?)";
 	private static final String INSERT_POST_TAG_QUERY = "INSERT INTO post_tag (post_id,tag_id) VALUES (?,?)";
@@ -119,6 +121,8 @@ public class PostRepository {
 			int postId = result.getInt(1);
 
 			post = new Post(postId, description, url, section, dateTime);
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 
 		TagRepository tagRepo = TagRepository.getInstance();
@@ -140,6 +144,8 @@ public class PostRepository {
 				ps.setInt(1, post.getId());
 				ps.setInt(2, id);
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		this.POSTS.put(post.getId(), post);
 		return post;
@@ -154,33 +160,27 @@ public class PostRepository {
 			throw new PostException(NOT_EXIST_POST_MEESAGE);
 		}
 
-		 User user = Session.getInstance().getUser();
-		 if (user.checkForRatedPostByPostId(postId)) {
-		 throw new PostException(ALREADY_RATED_POST_MESSAGE);
-		 }
-		 
-		 Post post = PostRepository.getInstance().getPostById(postId);
-		 post.addRating(grade);
-		 user.addRatedPost(post);
-		 
-		 
-		 int userId = user.getId();
-		 try(PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement(INSERT_POST_RATING_USER_QUERY)){
-			 ps.setInt(1, postId);
-			 ps.setInt(2, userId);
-			 ps.setInt(3, grade);
-			 ps.executeUpdate();
-		 }
-	}
-
-	
-	public int getLastId() {
-		if (this.POSTS == null || this.POSTS.size() == 0) {
-			return 0;
+		User user = Session.getInstance().getUser();
+		if (user.checkForRatedPostByPostId(postId)) {
+			throw new PostException(ALREADY_RATED_POST_MESSAGE);
 		}
 
-		return this.POSTS.keySet().stream().sorted((id1, id2) -> Integer.compare(id1, id2)).findFirst().get()
-				.intValue();
+		Post post = PostRepository.getInstance().getPostById(postId);
+		post.addRating(grade);
+		user.addRatedPost(post);
+
+		int userId = user.getId();
+		// Open connection to insert the grade
+		try (PreparedStatement ps = DatabaseConnection.getConnection()
+				.prepareStatement(INSERT_POST_RATING_USER_QUERY)) {
+			ps.setInt(1, postId);
+			ps.setInt(2, userId);
+			ps.setInt(3, grade);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println(MSG_SUCCESSFULY_GRADED);
 	}
 
 	public Post getPostById(int postId) {
@@ -191,107 +191,38 @@ public class PostRepository {
 		return this.POSTS.get(postId);
 	}
 
-	
 	public void listPostsByTagName(String tagName) {
-		CommentRepository commentsRepository = CommentRepository.getInstance();
-
-		// тука има малка промяна понеже вече постовете имат колекция от тагове(а самите
-		// постове си пазят само ид-то на таговете
-		// заради цикличния проблем при сериализация ако пазят само обекти).
-		// Та за да пресея всички постове, които съдържат подадения таг съм направил тоя
-		// помощен метод
-		// при филтрацията, който по всичките ид-та на тагове в поста бърка в таг
-		// репоситорито и връща имената
-		// на тези тагове и филтрирам само тези постове, които съдържат този таг
-		this.POSTS.values().stream().filter(p -> getTagNamesByTagIds(p.getTagIds()).contains(tagName))
-				.sorted((p1, p2) -> p2.getDate().compareTo(p1.getDate()))
-				.forEach(p -> System.out.println(p + System.lineSeparator()
-						+ getPrintedComments(commentsRepository.getCommentsByPostId(p.getId()))
-						+ System.lineSeparator()));
+		this.POSTS.values().stream().filter(p -> p.getTags().stream().anyMatch(t -> t.getName().equals(tagName)))
+				.sorted((p1, p2) -> p2.getDateTime().compareTo(p1.getDateTime())).forEach(p -> System.out.println(p));
 	}
 
 	public void listAllPostsSortedByLatest() {
-		CommentRepository commentsRepository = CommentRepository.getInstance();
-
-		this.POSTS.values().stream().sorted((p1, p2) -> p2.getDate().compareTo(p1.getDate()))
-				.forEach(p -> System.out.println(p + System.lineSeparator()
-						+ getPrintedComments(commentsRepository.getCommentsByPostId(p.getId()))
-						+ System.lineSeparator()));
+		this.POSTS.values().stream().sorted((p1, p2) -> p2.getDateTime().compareTo(p1.getDateTime()))
+				.forEach(p -> System.err.println(p));
 	}
 
 	public void listAllPostsSortedByRating() {
-		CommentRepository commentsRepository = CommentRepository.getInstance();
-
 		this.POSTS.values().stream().sorted((p1, p2) -> Integer.compare(p2.getRating(), p1.getRating()))
-				.forEach(p -> System.out.println(p + System.lineSeparator()
-						+ getPrintedComments(commentsRepository.getCommentsByPostId(p.getId()))
-						+ System.lineSeparator()));
+				.forEach(p -> System.out.println(p));
 	}
 
 	public void listAllPostsBySectionName(String sectionName) throws PostException {
 		Section section = SectionRepository.getInstance().getSectionByName(sectionName);
 
-		// тука правя проверка ако секцията не съществува хвърлям ексепшън и показвам
-		// като съобщение
-		// всичките налични секции от които може да се избере
 		List<String> allSectionNames = SectionRepository.getInstance().getAllSectionNames();
 		if (section == null) {
 			throw new PostException(INVALID_SECTION_NAME + String.join(", ", allSectionNames));
 		}
 
-		CommentRepository commentsRepository = CommentRepository.getInstance();
+		Set<Post> posts = section.getPost();
 
-		// тука взимам всичките ид-та на постовете в избраната секция и след това долу в
-		// stream-a
-		// филтрирам мапа-а с постове по ид-та, които се съдържат в сета който съм взел
-		// от секцията от която трябва
-		// да се листнат постовете след това взимам само value-тата от мапа понеже само
-		// те трябват
-		// сортирам по дата от най-нови към най-стари постове и другото е стандарно като
-		// в другите
-		// команди показвам всеки пост от секцията с всичките му коментари.
-		Set<Integer> postIds = section.getPostIds();
-
-		if (postIds.size() == 0) {
+		if (posts.size() == 0) {
 			System.out.println(NO_POST_MESSAGE);
 		} else {
-			this.POSTS.entrySet().stream().filter(kvp -> postIds.contains(kvp.getKey())).map(kvp -> kvp.getValue())
-					.sorted((p1, p2) -> p2.getDate().compareTo(p1.getDate()))
-					.forEach(p -> System.out.println(p + System.lineSeparator()
-							+ getPrintedComments(commentsRepository.getCommentsByPostId(p.getId()))
-							+ System.lineSeparator()));
+			posts.stream().filter(p -> p.getSection().getName().equals(sectionName))
+					.forEach(p -> System.out.println(p));
 		}
 
-	}
-
-	private Set<String> getTagNamesByTagIds(Set<Integer> tagIds) {
-		TagRepository tagRepository = TagRepository.getInstance();
-		Set<String> tagNames = new HashSet<>();
-
-		for (Integer tagId : tagIds) {
-			Tag tag = tagRepository.getTagById(tagId);
-
-			if (tag != null) {
-				tagNames.add(tag.getName());
-			}
-		}
-
-		return tagNames;
-	}
-
-	private String getPrintedComments(List<Comment> comments) {
-		if (comments.size() == 0) {
-			return NO_POST_COMMENTS_MESSAGE;
-		}
-
-		StringBuilder sb = new StringBuilder();
-		for (Comment comment : comments) {
-			sb.append("  -" + comment + System.lineSeparator());
-		}
-		sb.append("---------------------------------");
-		sb.append(System.lineSeparator());
-
-		return sb.toString();
 	}
 
 	public void getProcess() {
